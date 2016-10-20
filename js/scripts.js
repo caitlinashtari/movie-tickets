dc4d0bd9652ba5dee3540adb7d59b137b9adbd4b
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-10-19 17:45:23 -0700

    add to first conditional statment to include all keys and values.

--- js/scripts.js
+++ js/scripts.js
@@ -9,21 +9,23 @@ function Movie(title, time, date, age, price)  {
 
 Movie.prototype.getPrice = function () {
   if (this.title === "movie1") {
-    this.price = 5;
-
+    this.price += 5;
+    if ((this.time === "time1") || (this.time === "time2")){
+      this.price -= 1;
+      if ((this.date === "day1") || (this.date === "day2") || (this.date === "day3") || (this.date === "day4")) {
+        this.price -= 1;
+        if ((this.age <= 12) || (this.age >= 50))
+        this.price -= 1;
+      }
+    }
 
   } else if (this.title === "movie2") {
-    this.price = 4;
-
-
+    this.price += 4;
   } else if (this.title === "movie3") {
-    this.price = 3;
-
-
+    this.price += 3;
   } else {
-  this.price = 2;
-}
-
+    this.price += 2;
+  }
 }
 
 
